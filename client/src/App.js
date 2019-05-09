f9baeb1ed2a7a21283b035dd0c39d2807c94ce17
박세훈 <qkrsh0424@naver.com>
2019-05-09 11:04:13 +0900

    create customer.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,51 @@ import './App.css';
 
 import Customer from './Components/Customer';
 
+const customerdata = [
+  {
+    'id':1,
+    'image':"https://placeimg.com/64/64/1",
+    'name':'sehoon',
+    'birthday':'940424',
+    'gender':'male',
+    'job':'student'
+  },
+  {
+    'id':2,
+    'image':"https://placeimg.com/64/64/2",
+    'name':'binhee',
+    'birthday':'950110',
+    'gender':'female',
+    'job':'student'
+  },
+  {
+    'id':3,
+    'image':"https://placeimg.com/64/64/3",
+    'name':'john',
+    'birthday':'970424',
+    'gender':'male',
+    'job':'student'
+  },
+]
 class App extends Component{
   render(){
     return (
-      <div className="App">
-        <Customer/>
+      <div>
+        {
+          customerdata.map(c=> {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
       </div>
     );
   }
--- client/src/Components/Customer.js
+++ client/src/Components/Customer.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
 class Customer extends React.Component{
   render(){
     return (
       <div>
-        <h1>hello World this is Management Component</h1>
+        <h2>{this.props.name}({this.props.id})</h2>
+        <img src={this.props.image} alt="profile"/>
+        <p>{this.props.birthday}</p>
+        <p>{this.props.gender}</p>
+        <p>{this.props.job}</p>
       </div>
     );
   }
